d945dffe2b509ea2324bdcc9a28da5520fd8eae6
thanos vassilakis <thanos@infinigongroup.com>
2015-11-30 18:37:15 -0500

    added job and run models

--- djjobs/models.py
+++ djjobs/models.py
@@ -1,1 +1,55 @@
 # -*- coding: utf-8 -*-
+# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
+#
+# (c)2015  thanos vassilakis
+#
+
+from django.db import models
+from django.utils.text import slugify
+from autoslug import AutoSlugField
+
+class Job(models.Model):
+    TYPE_PENDING = 'AD HOC JOB'
+    TYPE_CRON = 'CRON JOB'
+    TYPE_SERVICE = 'SERVICE'
+    label = models.CharField(max_length=64)
+    slug = AutoSlugField(populate_from='label')
+    description = models.CharField(max_length=255)
+    run_type = models.CharField(max_length=1, choices= map(lambda x: (x[0],x), (TYPE_PENDING,TYPE_CRON, TYPE_SERVICE)), default = TYPE_CRON)
+    enabled = models.BooleanField(default=False)
+
+    def start(self):
+        self.enabled = True
+        self.save()
+        return Run.objects.create(
+                job = self
+            )
+
+    @property
+    def last_run(self):
+        return self.run.latest('ended')
+    
+    @property
+    def running(self):
+        return self.run_set.filter(state = Run.STATUS_RUNNING)
+        
+    @property
+    def latest(self):
+        return self.running.latest('ended')
+    @property
+    def run(self):
+        return self.run_set.filter(state__ne = Run.STATUS_RUNNING)
+    
+class Run(models.Model):
+    STATUS_RUNNING = 'RUNNING'
+    STATUS_COMPLETED = 'COMPLETED'
+    STATUS_ABORTED = 'ABORTED'
+    job = models.ForeignKey('Job')
+    started = models.DateTimeField(auto_now_add=True)
+    ended = models.DateTimeField(null=True, blank=True)
+    state  = models.CharField(max_length=1, choices= map(lambda x: (x[0],x), (STATUS_RUNNING, STATUS_COMPLETED, STATUS_ABORTED)), default = STATUS_RUNNING)
+    errors  = models.TextField(default='')
+
+
+
+    
--- tests/test_models.py
+++ tests/test_models.py
@@ -18,8 +18,52 @@ class TestDjjobs(TestCase):
     def setUp(self):
         pass
 
-    def test_something(self):
-        pass
+    def test_creating_a_job(self):
+        from django.utils.text import slugify
+        label = 'test_job'
+        job =  models.Job.objects.create(
+            label = label,
+            description = 'a test job for testing slug'
+        )
+        assert job.slug == slugify(label), "slugify not working: %s" % `job.slug`
+        assert job.run_type == job.TYPE_CRON, "run type default wrong: %s" % `job.run_type`
+        assert job.enabled == False, "Should be disabled but is %s" % job.enabled
+
+    def test_starting_a_job(self):
+        label = 'test_job'
+        job =  models.Job.objects.create(
+            label = label,
+            description = 'a test job for testing slug'
+        )
+        run = job.start()
+        assert run.job == job, "job was not set for run %s" % run.pk
+        assert run.state == run.STATUS_RUNNING, "state not switcted to running but is  %s" % `run.state`
+        assert run.started != None, "stated should  when it should not be set but is %s" % `run.started`
+        assert run.ended == None, "ended when it should not be set but is %s" % `run.ended`
+        assert run.errors == '', "Errors should not be set  but is %s" % `run.errors`
+        assert job.enabled == True, "Should be enabled but is %s" % job.enabled
+        assert job.latest == run, "last run should be completed %s" % run
+
+    # def test_creating_a_run(self):
+    #     job =  models.Job.objects.create(
+    #         label = label,
+    #         description = 'a test job for testing slug'
+    #     )
+    #     run = models.Run.objects.create(
+    #             job = models.ForeignKey('Job')
+    #             started = models.DateTimeField(auto_now_add=True)
+    #             ended = models.DateTimeField(null=True)
+    #             state  = models.CharField(max_length=1, choices= map(lambda x: (x[0],x), (STATUS_PENDING,STATUS_RUNNING, STATUS_COMPLETED, STATUS_ABORTED)), default = STATUS_PENDING)
+    #             errors  = models.TextField(default='')
+    #         )
+    #     assert run.job == job, "job was not set for run %s" % run.pk
+    #     assert run.state == run.STATUS_RUNNING, "state not switcted to running %s" % run.state
+    #     assert run.started != None, "stated should  when it should not be set %s" % run.ended
+    #     assert run.ended == None, "ended when it should not be set %s" % run.ended
+    #     assert run.errors == None, "Errors should not be set %s" % run.errors
+
+
+
 
     def tearDown(self):
         pass
